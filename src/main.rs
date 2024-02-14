705b21ece0e6f531b3f8e7bd9c1c12b490a3bad6
EYRAUD Come <ce.eyraud@gmail.com>
2024-02-14 17:09:49 +0100

    Fixing things

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,39 @@
-
-use std::{net::TcpListener, thread::spawn};
-
+use std::{ net::TcpListener, thread::spawn };
 
 use rand::prelude::*;
 
 use tungstenite::Message;
-use serde::{Deserialize, Serialize};
+use serde::{ Deserialize, Serialize };
 
 #[derive(Serialize, Deserialize)]
 struct Resp {
-    direction:String,
-    rate:i16,
-    text:char
+    direction: String,
+    rate: i16,
+    text: char,
 }
 
-fn build_response()-> Result<String, serde_json::Error>{
-    let mut rng = rand::thread_rng();
-    let rand_tuple: (bool, i16, u8) = rng.gen::<(bool,i16,u8)>();
-    let dir = if rand_tuple.0 {
-        "in"
-    } else {
-        "out"
-    };
-    let r: Resp = Resp{direction: dir.to_owned(),
-        rate:rand_tuple.1.to_owned(),
-        text:rand_tuple.2.to_owned() as char,
-    };
-    let j: Result<String, serde_json::Error> = serde_json::to_string(&r);
-    return j;
-}
-
-fn process_query(msg:&Message)-> Message{
-    match msg {
-        Message::Text(_) => {
-            let text = build_response().unwrap();
-            return Message::Text(text.to_owned());
-        }
-        _ => {
-            return Message::Text("No".to_owned());
+impl Resp {
+    // Custom constructor to create a default Resp instance
+    pub fn new() -> Self {
+        let mut rng = rand::thread_rng();
+        let rand_tuple: (bool, i16, u8) = rng.gen::<(bool, i16, u8)>();
+        let dir: &str = if rand_tuple.0 { "in" } else { "out" };
+        Self {
+            direction: dir.to_owned(),
+            rate: rand_tuple.1.to_owned(),
+            text: rand_tuple.2.to_owned() as char,
         }
     }
+}
 
+fn build_response() -> Result<String, serde_json::Error> {
+    let r: Resp = Resp::new();
+    return serde_json::to_string(&r);
+}
+
+fn process_query() -> Message {
+    let text = build_response().unwrap();
+    return Message::Text(text.to_owned());
 }
 
 fn main() {
@@ -54,7 +47,7 @@ fn main() {
             loop {
                 let msg = websocket.read().unwrap();
                 if msg.is_binary() || msg.is_text() {
-                    websocket.send(process_query(&msg)).unwrap();
+                    websocket.send(process_query()).unwrap();
                 }
             }
         });
